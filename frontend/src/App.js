f1e3435fe2571a9df0a630f23709cded6b93fe80
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-12-21 11:03:34 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import UploadBill from "./UploadBill";
+import BillUploader from "./components/BillUploader";
 
 export default function App() {
-  return <UploadBill />;
+  return (
+    <div style={{ maxWidth: "600px", margin: "2rem auto", padding: "0 1rem" }}>
+      <h1>Explain My Bill</h1>
+      <p>Upload your medical or utility bills and get an easy-to-understand explanation.</p>
+      <BillUploader />
+    </div>
+  );
 }
